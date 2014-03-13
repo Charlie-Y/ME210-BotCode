76a3d8ef5b7087082ef94e74ffff22180e4d3bdc
Charlie Yang <charlesy@stanford.edu>
2014-03-13 15:21:28 -0700

    more cleanup

--- Execute/Execute.ino
+++ Execute/Execute.ino
@@ -2,19 +2,13 @@
 
 
 void setup(){
-    // call all the initialization code for all the libraries
     Serial.begin(9600);
     Serial.println("Starting the BotCoin code...");
-    // log_states();
     setup_states();
 }
 
 void loop(){
     execute_current_state();
-    // delay(100);
 }
 
 
-// todo - implement motor controls
-// todo - implement button pressing controls
-// todo - implement coin controls
